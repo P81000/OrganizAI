3ed23790409ba610173dd5b1d2d4a2d263b74c40
Thiago Roberto <thiagorfrocha16@gmail.com>
2023-10-03 23:54:20 -0300

    Implementação do CRUD de Tarefas

--- src/main/java/com/organizai/app/controller/EventoController.java
+++ src/main/java/com/organizai/app/controller/EventoController.java
@@ -2,6 +2,8 @@ package com.organizai.app.controller;
 
 import java.util.List;
 
+import com.organizai.app.tarefa.Tarefa;
+import com.organizai.app.tarefa.TarefaRepository;
 import com.organizai.app.usuario.Usuario;
 import com.organizai.app.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,8 @@ public class EventoController {
 
     @Autowired
     private EventoRepository eventoRepository;
+    @Autowired
+    private TarefaRepository tarefaRepository;
 
     @GetMapping
     public String getEvento(Model model) {
@@ -50,6 +54,7 @@ public class EventoController {
             // Retorne o evento salvo e o status 201 Created
             return ResponseEntity.status(HttpStatus.CREATED).body(eventoSalvo);
       }*/
+
     @PostMapping("/{email}")
     public ResponseEntity<Evento> createEvento(@PathVariable String email, @RequestBody Evento novoEvento) {
 
@@ -107,4 +112,58 @@ public class EventoController {
         return ResponseEntity.noContent().build();
     }
 
+    @PostMapping("/{id}") // Fiz por ID do evento, mas não sei se é o ideal.
+    public ResponseEntity<Tarefa> createTarefa(@PathVariable int id_evento, @RequestBody Tarefa novaTarefa) {
+
+        // Verificar se é válido
+        if (novaTarefa == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        //System.out.println("tarefa: " + novoTarefa.getCorpo());
+
+        Evento evento = eventoRepository.getById(id_evento);
+        if (evento == null) {
+            return ResponseEntity.notFound().build(); // Retorna status 404 Not Found se o evento não existe
+        }
+
+        evento.setTarefas(novaTarefa);
+        novaTarefa.set_evento(evento);
+        //System.out.println("tarefa: " + novoTarefa.getEvento());
+
+        Tarefa tarefaSalva = tarefaRepository.save(novaTarefa);
+        //eventoRepository.save();
+
+        // Retornar a tarefa salva e o status 201 Created
+        return ResponseEntity.status(HttpStatus.CREATED).body(tarefaSalva);
+    }
+
+    @PutMapping("/{id}") // Mapeia para tarefas/{id} com método PUT
+    public ResponseEntity<Tarefa> updateTarefa(@PathVariable Integer id, @RequestBody Tarefa tarefaAtualizada) {
+        // Verifique se a tarefa a ser atualizado existe
+        if (!tarefaRepository.existsById(id)) {
+            return ResponseEntity.notFound().build(); // Retorna status 404 Not Found se a tarefa não existe
+        }
+
+        // Verificar se é valido IMPLEMENTAR
+        if (tarefaAtualizada == null) {
+            return ResponseEntity.badRequest().build(); // Retorna status de BadRequest se a tarefa for inválida
+        }
+
+        Tarefa tarefaAtualizadaNoBanco = tarefaRepository.save(tarefaAtualizada);
+
+        // Tarefa atualizada e o status 200 OK
+        return ResponseEntity.ok(tarefaAtualizadaNoBanco);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTarefa(@PathVariable Integer id) {
+
+        if (!tarefaRepository.existsById(id)) {
+            return ResponseEntity.notFound().build(); // Retorna status 404 Not Found
+        }
+
+        tarefaRepository.deleteById(id);
+
+        // Retorne o status 204 No Content (sem conteúdo) para indicar a exclusão bem-sucedida
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/organizai/app/evento/EventoRepository.java
+++ src/main/java/com/organizai/app/evento/EventoRepository.java
@@ -1,7 +1,9 @@
 package com.organizai.app.evento;
 
+import com.organizai.app.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventoRepository extends JpaRepository<Evento, Integer> {
-      
+    @Override
+    Evento getById(Integer integer);
 }
