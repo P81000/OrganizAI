089d7b4f4457004e81c37d972ce8c0e8ffd60074
João Barbosa <35549156+Joaovcbarbosa@users.noreply.github.com>
2023-11-09 11:03:41 -0300

    Diminuindo número máximo de novos tokens para menor consumo da API

--- docs/tests/llmtest/index.js
+++ docs/tests/llmtest/index.js
@@ -19,7 +19,7 @@ async function runModel() {
         prompt: "return a .json according to the template below, this .json is from a POST to create an event, dont respond anything besides the .json data:\ncreate an event \"go to the dentist\" and suggest start_date and end_date\nPOST example:\n{\n\"titulo\": \"Team Meeting\",\n\"description\": \"Discussion about quarterly goals\",\n\"start_date\": \"2023-09-20T10:00:00\",\n\"end_date\": \"2023-09-20T12:00:00\",\n\"location\": \"Meeting Room 1\",\n\"event_status\": \"Scheduled\",\n\"user_id\": \"123\",\n\"notification_id\": \"456\",\n\"id_info_clima\": \"789\",\n\"id_info_trajeto\": \"101\"\n}\nDon't use any of the following Dates:\n2023-11-07T10:00:00\n2023-11-08T10:00:00\n2023-11-09T10:00:00\n2023-11-10T10:00:00\n2023-11-11T10:00:00\n2023-11-12T10:00:00\n2023-11-13T10:00:00",
         temperature: 0.5,
         //limite de tokens para a resposta, cada palavra tem cerca de 3 a 4 tokens
-        max_new_tokens: 1000,
+        max_new_tokens: 512,
         min_new_tokens: -1
       }
     }
