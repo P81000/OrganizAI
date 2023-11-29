8d85dd28d0b36589580cc9cd718157b02d89c0d1
Leonardo <scadelai@unifesp.br>
2023-11-29 12:36:02 -0300

    Melhora do botão de Cadastro de Evento

--- src/frontend-src/src/components/EventComponent.vue
+++ src/frontend-src/src/components/EventComponent.vue
@@ -1,35 +1,44 @@
 <template>
   <div id="cadastroEventoForm">
     <h2>Cadastro de Evento</h2>
+
     <form id="eventoForm" class="custom-login-form" @submit.prevent="submitEvento">
-      <label for="titulo">Título:</label>
-      <input type="text" v-model="form.titulo" class="custom-input" required><br>
+      <div class="form-row">
+        <div class="form-group">
+          <label for="titulo">Título:</label>
+          <input id="titulo" v-model="form.titulo" class="custom-input" required>
+        </div>
 
-      <label for="descricao">Descrição:</label>
-      <input type="text" v-model="form.descricao" class="custom-input" required><br>
+        <div class="form-group">
+          <label for="descricao">Descrição:</label>
+          <textarea id="descricao" v-model="form.descricao" class="custom-input" required></textarea>
+        </div>
 
-      <label for="dataInicio">Data de Início:</label>
-      <input type="datetime-local" v-model="form.data_inicio" class="custom-input" required><br>
+          <div class="form-column">
+            <div class="form-group">
+            <label for="dataInicio">Início:</label>
+            <input type="datetime-local" v-model="form.data_inicio" class="custom-input" required>
+          </div>
 
-      <label for="dataFim">Data de Fim:</label>
-      <input type="datetime-local" v-model="form.data_fim" class="custom-input" required><br>
+          <div class="form-group">
+            <label for="dataFim">Fim:</label>
+            <input type="datetime-local" v-model="form.data_fim" class="custom-input" required>
+          </div>
 
-      <label for="localizacao">Localização:</label>
-      <input type="text" v-model="form.localizacao" class="custom-input" required><br>
+          <div class="form-group">
+            <label for="localizacao">Localização:</label>
+            <input type="text" v-model="form.localizacao" class="custom-input" required>
+          </div>
 
-      <label for="idNotificacao">ID de Notificação:</label>
-      <input type="text" v-model="form.id_notificacao" class="custom-input"><br>
-
-      <label for="idInfoClima">ID de Informação de Clima:</label>
-      <input type="text" v-model="form.id_info_clima" class="custom-input"><br>
-
-      <label for="idInfoTrajeto">ID de Informação de Trajeto:</label>
-      <input type="text" v-model="form.id_info_trajeto" class="custom-input"><br>
-
-      <button type="submit" class="custom-button">Salvar Evento</button>
-      <button type="button" class="custom-button" @close="$emit('close')">Fechar</button>
+          <div class="form-group">
+            <label for="idNotificacao">ID de Notificação:</label>
+            <input type="text" v-model="form.id_notificacao" class="custom-input">
+          </div>
+        </div>
+      </div>
     </form>
-    <button @click="deleteEvento()"></button>
+    <!--button @click="createEvento()">Criar Evento</button!-->
+    <button @click="deleteEvento()">Deletar Evento</button>
   </div>
 </template>
 
@@ -98,6 +107,92 @@ const deleteEvento = async () => {
   } catch(error){
     console.log("Error: ", error);
   }
-
 };
 </script>
+
+<style scoped>
+.form-group {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+
+.custom-input {
+  width: 75%;
+}
+
+label[for="descricao"] {
+  display: flex;
+  align-items: center;
+}
+
+#descricao {
+  margin-left: 5px;
+}
+
+label[for="titulo"] {
+  display: flex;
+  align-items: center;
+}
+
+#titulo {
+  margin-left: 5px;
+}
+
+#cadastroEventoForm {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 15px;
+  background-color: #f9f9f9;
+  border-radius: 5px;
+}
+
+#eventoForm {
+  display: grid;
+  grid-gap: 10px;
+}
+
+.custom-input {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+
+.custom-login-form label {
+  font-weight: bold;
+}
+
+.custom-button {
+  margin-top: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+.custom-button:hover {
+  background-color: #0056b3;
+}
+
+.form-row {
+  flex: 1;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.form-column {
+  flex: 1;
+  margin-right: 10px;
+}
+
+.form-column:last-child {
+  margin-right: 0;
+}
+</style>
